2f471f8d4f33087539048e62879d7e7a642c23b9
back2nix <back2nix@list.ru>
2024-12-16 14:27:08 +0300

    save

--- adb_info.sh
+++ adb_info.sh
@@ -8,7 +8,19 @@ adb devices
 adb connect 192.168.0.100
 
 # нажать на экран
-adb shell input tap 1024 512 
+adb shell input tap 1024 512
 
 # для eperia z5 выставить 4л принудительно
 adb shell wm density %target dpi% ( для нашего разрешения по-моему 806)
+
+### Вытащить apk на комп
+```bash
+adb shell pm list packages | grep com.example.app
+adb shell pm path com.example.app
+adb pull /data/app/com.example.app-1.apk C:\путь\куда\сохранить\app.apk
+```
+- или
+
+```bash
+adb shell pm path com.example.app | sed 's/package://g' | xargs -I {} adb pull {} app.apk
+```
