3c25b84b27b8552f6a0aac2dc889ad9b3085145e
back2nix <back2nix@list.ru>
2025-07-14 14:12:45 +0300

    mail

--- mail_info.sh
+++ mail_info.sh
@@ -5,3 +5,7 @@ curl -s https://mailinabox.email/setup.sh | sudo bash
 
 - free mail redirect cloudflare + gmail.com
 https://www.youtube.com/watch?v=NmXWA08ly_s
+
+
+- duckduckgo mail
+https://duckduckgo.com/email/settings/autofill
